2b2f530ef50890e14f65eab5d9e044cdcec3ed0e
tiwariaastha <aasthanityanandtiwari94@gmail.com>
2021-07-11 22:18:33 +0530

    Reactt 1st row

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import ExpenseItem from './components/ExpenseItems';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <ExpenseItem></ExpenseItem>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+import  './ExpenseItem.css';
+function expenseItem (){
+    return (
+    <div className ='expense-item'>
+        <div> 20 Nov 2020</div>
+    <div className="expense-item_description">
+        <h2>car insurance</h2>
+    <div className ="expense-item_price>"> 2000</div>
+    </div>
+    </div>
+    );
+}
+export default expenseItem;
+
